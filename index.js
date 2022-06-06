28ef48f8232a7f28071e42fa0579ef1e70cb810d
MubiynDev <mubiynsafiyulloh@icloud.com>
2022-06-06 12:49:30 +0500

    1

---
+++ index.js
@@ -0,0 +1,356 @@
+let DATA = {
+
+    chats: [
+        {
+            id: 1,
+            first_name: "Muhammed",
+            last_name: "Ali",
+            avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVEjoPV0LSFFVpaHXtFwdDGOczYXCIwZmi-Q&usqp=CAU",
+            bio: "Believe yourself!",
+            phone_number: "+44 777 777 77 77",
+            user_name: "@Ali_701",
+            groups_common: 4,
+            shared_links: 15,
+            shared_files: 1,
+            shared_photos: 20,
+            shared_voice: 15,
+            activity: "online",
+            messages: [
+                {
+                    id: 1,
+                    is_from_me: false,
+                    text: "Hello",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+                {
+                    id: 2,
+                    is_from_me: true,
+                    text: "Hi",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+                {
+                    id: 3,
+                    is_from_me: false,
+                    text: "How are you ?",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+                {
+                    id: 4,
+                    is_from_me: true,
+                    text: " and you ?",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+            ]
+        },
+
+
+        {
+            id: 2,
+            first_name: "Polat",
+            last_name: "Alemdar",
+            avatar: "http://sun9-57.userapi.com/s/v1/if2/KZb3_VXtPmh3OdI0o4GFsN8rGwcgCxIz6WmHtqUkeLQVWy3Std7NkeyBx5w2a6HuRxgiyeSP6J2XyUK9alGsln5i.jpg?size=400x400&quality=96&crop=43,0,426,426&ava=1",
+            bio: "Sonunu dusenen kahraman olamaz!",
+            phone_number: "+44 777 777 77 77",
+            user_name: "@can_polat",
+            groups_common: 4,
+            shared_links: 15,
+            shared_files: 1,
+            shared_photos: 20,
+            shared_voice: 15,
+            activity: "last seen recently",
+            messages: [
+                {
+                    id: 1,
+                    is_from_me: false,
+                    text: "selamun aleykum",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+                {
+                    id: 2,
+                    is_from_me: true,
+                    text: "aleykum selam ne haber pusu devami ne zaman?",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+                {
+                    id: 3,
+                    is_from_me: false,
+                    text: "bitti dizi yaslandik artik",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+                {
+                    id: 4,
+                    is_from_me: true,
+                    text: "ok",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+            ]
+        },
+
+
+        {
+            id: 3,
+            first_name: "Tim",
+            last_name: "Aroon",
+            avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTa31xKmnI6rYS9nSNXJNOewF7o1E49L6r_Q&usqp=CAU",
+            bio: "s.o.s",
+            phone_number: "+44 777 777 77 77",
+            user_name: "@ta_777",
+            groups_common: 4,
+            shared_links: 15,
+            shared_files: 1,
+            shared_photos: 20,
+            shared_voice: 15,
+            activity: "Last seen 1 hour ago",
+            messages: [
+                {
+                    id: 1,
+                    is_from_me: true,
+                    text: "Hello",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+                {
+                    id: 2,
+                    is_from_me: false,
+                    text: "Hi",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+                {
+                    id: 3,
+                    is_from_me: false,
+                    text: "How are you ?",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+                {
+                    id: 4,
+                    is_from_me: false,
+                    text: "im fine thank you",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+            ]
+        },
+
+
+        {
+            id: 4,
+            first_name: "Alex",
+            last_name: "77",
+            avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNfvr-e3dE--_v7q7bQFX3ZEn2aSJtkri3pg&usqp=CAU",
+            bio: "Believe yourself!",
+            phone_number: "+44 777 777 77 77",
+            user_name: "@Alexxxx01",
+            groups_common: 4,
+            shared_links: 15,
+            shared_files: 1,
+            shared_photos: 20,
+            shared_voice: 15,
+            activity: "online",
+            messages: [
+                {
+                    id: 1,
+                    is_from_me: false,
+                    text: "Hello",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+                {
+                    id: 2,
+                    is_from_me: true,
+                    text: "Hi",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+                {
+                    id: 3,
+                    is_from_me: false,
+                    text: "How are you ?",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+                {
+                    id: 4,
+                    is_from_me: true,
+                    text: "whats up bro?",
+                    time: new Date(2022, 6, 2, 11, 10, 05).getTime()
+                },
+            ]
+        },
+
+
+        {
+            id: 5,
+            first_name: "Alisher",
+            last_name: "07",
+            avatar: "https://broncolor.swiss/assets/img/Stories/Inspiration/Daniel-Radcliffe/_contentWithShareBar23/DANIEL-RADCLIFFE-featured.jpg",
+            bio: "lorem ipsum dolor sit",
+            phone_number: "+998 77 234 47 77",
+            user_name: "@Alisher_07",
+            groups_common: 4,
+            shared_links: 15,
+            shared_files: 1,
+            shared_photos: 20,
+            shared_voice: 15,
+            activity: "last seen 6 hours ago",
+            messages: [
+                {
+                    id: 1,
+                    is_from_me: false,
+                    text: "nimagap",
+                    time: new Date(2022, 6, 1, 11, 10, 05).getTime()
+                },
+                {
+                    id: 2,
+                    is_from_me: true,
+                    text: "tinchli ozindachi",
+                    time: new Date(2022, 6, 1, 11, 10, 05).getTime()
+                },
+                {
+                    id: 3,
+                    is_from_me: false,
+                    text: "tinch",
+                    time: new Date(2022, 6, 1, 11, 10, 05).getTime()
+                },
+                
+            ]
+        },
+
+
+        
+
+        
+    ]
+}
+
+!localStorage.getItem("DATA") && localStorage.setItem("DATA", JSON.stringify(DATA));
+
+let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+const renderLists = () => {
+   DATA.chats.forEach((item, index) => {
+       let newList = `
+       <li class="chats-item" id="${item.id}" username="${item.user_name}">
+                    <img src="${item.avatar}"  alt="" class="avatar rounded-circle float-start me-2">
+                    <div class="chat-container d-flex align-items-center justify-content-between">
+                        <h5 class="user-name">${item.first_name + ' ' + item.last_name}</h5>
+                        <span class="${item.messages[item.messages.length-1].is_from_me ? "bi bi-check-all ms-auto me-1 text-info" : ""}"></span>
+                        <span class="time">${weekDays[new Date(item.messages[item.messages.length - 1].time).getDay()]}</span>
+                        
+                    </div>
+                    <div class="chat-item-container d-flex">
+                    <p class="message text-truncate" style="max-width: 250px;">${item.messages[item.messages.length-1].text}</p>
+                    <span class="${!item.messages[item.messages.length-1].is_from_me ? "unread-message ms-auto badge bg-info" : "hidden"}">+1</span>
+                    </div>
+                </li>
+       `
+       document.querySelector('.chats-ul').innerHTML += newList;
+
+
+   })
+   
+}
+renderLists()
+
+let parsedData = JSON.parse(localStorage.getItem("DATA"))
+let newChats = document.querySelectorAll('.chats-item');
+let inputMessage = document.querySelector('#send-message');
+
+
+
+
+
+
+  
+const renderChats = () => {
+    
+
+    newChats.forEach(el => {
+    
+        el.addEventListener('click', e => {
+            let elID = el.id - 0;
+            parsedData.chats.forEach(i => {
+                    if(elID === i.id && i){
+                        
+                    //   document.querySelector('.unread-message').classList.add("hidden") 
+
+                      document.querySelector('.nav-avatar').src = `${i.avatar}`
+                      document.querySelector('.username').innerHTML = `${i.first_name} ${i.last_name}`  
+                      document.querySelector('.last-seen').innerHTML = `${i.activity}`
+                      
+                      document.querySelector('.chats-content-body').innerHTML = ""
+
+                      i.messages.forEach(message => {
+                        document.querySelector('.chats-content-body').innerHTML += `
+                        <div class="${message.is_from_me ? "from-me" : "messages" }">
+                            <p class="message-item"> ${message.text}
+                               <div class="msg-info ">
+                                 <span class="is-checked bi bi-check-all"></span>
+                                 <span class="send-time">${new Date(message.time).getHours()}:${new Date(message.time).getMinutes()}</span>
+                               </div>
+                            </p>
+                        </div>
+                        
+                        
+                        
+                        `  
+
+
+                      })
+                
+                      document.querySelector('.chats-content-footer').classList.remove('hidden')
+                      
+                      inputMessage.focus()
+                      
+                      inputMessage.addEventListener('keyup', (e) => {
+                      
+                        let value = e.target.value.trim();
+                             if(value){
+                                 document.querySelector('.sender-icon').classList.remove("bi-mic-fill")
+                                 document.querySelector('.sender-icon').classList.add("bi-send-fill")
+                             }else{
+                                document.querySelector('.sender-icon').classList.remove("bi-send-fill")
+                                document.querySelector('.sender-icon').classList.add("bi-mic-fill")
+                      
+                             }
+                      })                  
+
+                   
+
+        document.querySelector('.sender-icon').addEventListener('click',() => {
+
+        })
+                i.messages.push({
+                        id: parsedData.chats.length,
+                        is_from_me: true,
+                        text: inputMessage.value,
+                        time: new Date().getTime()
+                })
+
+
+
+                     
+
+
+
+                }
+            })
+           
+        })
+    })
+    
+    
+}
+
+renderChats()
+
+
+
+// const addNewMessage = () => {
+   
+// }
+// addNewMessage()
+console.log(parsedData.chats)
+
+
+
+// chat.messages.push({
+//     id: parsedData.chats.length + 1,
+//     is_from_me: true,
+//     text: inputValue,
+//     time: `${new Date().getHours}:${new Date().getMinutes}`
+// })
