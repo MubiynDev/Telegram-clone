79cd6f308df7ab93659d7329a3e6c2c2ea4c88e3
MubiynDev <mubiynsafiyulloh@icloud.com>
2022-06-07 22:38:39 +0500

    sad

--- index.js
+++ index.js
@@ -211,6 +211,37 @@ let DATA = {
         },
 
 
+
+        {
+            id: 6,
+            first_name: "Mubiyn",
+            last_name: "Sofiyyulloh",
+            avatar: "https://mubiyndev.github.io/images/photo_2022-01-24_14-55-04.jpg",
+            bio: "",
+            phone_number: "+998 88 831 00 09",
+            user_name: "@Binsofiyyulloh",
+            groups_common: 4,
+            shared_links: 15,
+            shared_files: 1,
+            shared_photos: 20,
+            shared_voice: 15,
+            activity: "",
+            messages: [
+                {
+                    id: 1,
+                    is_from_me: true,
+                    text: "This is Saved Messages section. You can use it for your own private cloud. Enjoy Telegram :)",
+                    time: new Date(2022, 6, 1, 11, 10, 05).getTime()
+                },
+                
+                
+            ]
+        },
+
+
+        
+
+
         
 
         
@@ -246,9 +277,11 @@ const renderLists = () => {
 }
 renderLists()
 
-let parsedData = JSON.parse(localStorage.getItem("DATA"))
 let newChats = document.querySelectorAll('.chats-item');
 let inputMessage = document.querySelector('#send-message');
+
+let activeChatID;
+let targetChatID;
 
 
 
@@ -260,14 +293,21 @@ const renderChats = () => {
     
 
     newChats.forEach(el => {
-    
-        el.addEventListener('click', e => {
-            let elID = el.id - 0;
-            parsedData.chats.forEach(i => {
-                    if(elID === i.id && i){
-                        
-                    //   document.querySelector('.unread-message').classList.add("hidden") 
+        let parsedData = JSON.parse(localStorage.getItem("DATA"))
 
+        el.addEventListener('click', e => {
+            // let elID = el.id - 0;
+            let activeChatID = e.currentTarget.id - 0;
+           
+            parsedData.chats.forEach(i => {
+                
+                    if(activeChatID === i.id && i){
+                        console.log(i.id, activeChatID)
+
+
+                        
+                    
+                      document.querySelector('.chat-info').setAttribute("id", i.id)
                       document.querySelector('.nav-avatar').src = `${i.avatar}`
                       document.querySelector('.username').innerHTML = `${i.first_name} ${i.last_name}`  
                       document.querySelector('.last-seen').innerHTML = `${i.activity}`
@@ -275,6 +315,7 @@ const renderChats = () => {
                       document.querySelector('.chats-content-body').innerHTML = ""
 
                       i.messages.forEach(message => {
+                        
                           
                         document.querySelector('.chats-content-body').innerHTML += `
                         <div class="${message.is_from_me ? "from-me" : "messages" }">
@@ -311,46 +352,54 @@ const renderChats = () => {
                       })                  
 
                    
+        renderChats()
 
         document.querySelector('.sender-icon').addEventListener('click',() => {
-            let newMsg = {
-                id: parsedData.chats.length,
-                is_from_me: true,
-                text: inputMessage.value,
-                time: new Date().getTime()
-            }
+                if(inputMessage.value){
+                    targetChatID = parsedData.chats.find(item => item.id  === activeChatID)
+                if(activeChatID === i.id && targetChatID){
+                    i.messages.push({
+                        id: i.messages[i.messages.length -1].id + 1,
+                        is_from_me: true,
+                        text: inputMessage.value,
+                        time: new Date().getTime()
+                    })
+                    localStorage.setItem("DATA", JSON.stringify(parsedData))
+                }
+                   
 
-            if(inputMessage.value){
-                i.messages.push(newMsg)
-
-
-            document.querySelector('.chats-content-body').innerHTML += `
-                        <div class="from-me">
-                            <p class="message-item"> ${newMsg.text}
-                               <div class="msg-info ">
-                                 <span class="is-checked bi bi-check-all"></span>
-                                 <span class="send-time">${new Date(newMsg.time).getHours()}:${new Date(newMsg.time).getMinutes()}</span>
-                               </div>
-                            </p>
-                        </div>
-                        
-                          ` 
-                          localStorage.setItem("DATA", JSON.stringify(parsedData))
-
-
-            }
+    
+                document.querySelector('.chats-content-body').innerHTML += `
+                            <div class="from-me">
+                                <p class="message-item"> ${i.messages[i.messages.length-1].text}
+                                   <div class="msg-info ">
+                                     <span class="is-checked bi bi-check-all"></span>
+                                     <span class="send-time">${new Date(i.messages[i.messages.length-1].time).getHours()}:${new Date(i.messages[i.messages.length-1].time).getMinutes()}</span>
+                                   </div>
+                                </p>
+                            </div>
+                            
+                              ` 
+    
+    
+                
+                }
+            
         
              
             
 
             inputMessage.value = ""
             inputMessage.focus()
+            document.querySelector('.sender-icon').classList.remove("bi-send-fill")
+            document.querySelector('.sender-icon').classList.add("bi-mic-fill")
+
 
 
 
         })
 
-        
+       
                 
 
 
@@ -362,7 +411,9 @@ const renderChats = () => {
                 }
             })
            
-        })
+        })       
+        //  console.log(parsedData.chats)
+
     })
     
     
@@ -373,7 +424,109 @@ renderChats()
 
 
 
+// addNewMessage()
 
 
 
-console.log(parsedData.chats)
+//------------------------ modals ------------------
+
+let showSidebar = () => {
+    document.querySelector('.menu').addEventListener('click', (e) => {
+        document.querySelector('.sidebar-modal').style.display = "block"
+    
+       window.addEventListener('click', e => {
+           if(e.target.classList == 'sidebar-modal'){
+            document.querySelector('.sidebar-modal').style.display = "none"
+           }
+       })
+       document.querySelector('.sidebar-img').src = `${DATA.chats[DATA.chats.length-1].avatar}`
+       document.querySelector('.myname').innerHTML = `${DATA.chats[DATA.chats.length-1].first_name} ${DATA.chats[DATA.chats.length-1].last_name}`
+       document.querySelector('.mynumber').innerHTML = `${DATA.chats[DATA.chats.length-1].phone_number}`
+    
+    
+    
+    })
+}
+showSidebar()
+
+let showUserInfo = () => {
+
+    document.querySelector('.chat-info').addEventListener('click', e => {
+        let parsedData = JSON.parse(localStorage.getItem("DATA"))
+    
+       
+    
+        parsedData.chats.forEach(chat => {
+            
+            if(chat.id === e.currentTarget.id - 0 && e.currentTarget) {
+                
+                document.querySelector('.user-info-modal__content').innerHTML = `
+                
+                <div class="chat-info d-flex align-items-center mb-3 px-4">
+                <img src="${chat.avatar}"  alt="" class="avatar avatar2 nav-avatar rounded-circle float-start me-4">
+               <div class="info-wrapper d-flex flex-column">
+                <h5 class="username m-0">${chat.first_name} ${chat.last_name}</h5>
+                <small class="last-seen text-info">${chat.activity}</small>
+                <span class="bi bi-x"></span>
+               </div>
+            </div> 
+            
+            <div class="bio-wrapper">
+                <h7 class="mobile">${chat.phone_number}</h7>
+                <p class="mb-2 text-secondary">Mobile</p>
+                <h7 class="bio">${chat.bio}</h7>
+                <p class="mb-2 text-secondary">Bio</p>
+                <h7 class="username-modal">${chat.user_name}</h7>
+                <p class="text-secondary">Username</p>
+            </div>
+    
+            <div class="sharing-wrapper">
+                <ul class="m-0 p-0">
+                    <li class="shared-items "><i class="bi bi-folder2"></i> ${chat.shared_files} Files</li>
+                    <li class="shared-items "><i class="bi bi-image"></i> ${chat.shared_photos} Photos</li>
+                    <li class="shared-items "><i class="bi bi-link"></i> ${chat.shared_links} Links</li>
+                    <li class="shared-items"><i class=" bi bi-mic"></i> ${chat.shared_voice} Voice</li>
+                    
+                </ul>
+            </div>
+    
+            <div class="edition-wrapper">
+                <ul class="m-0 p-0">
+                    <li class="edition-items"><i class=" bi bi-pencil"></i> Edit Contact</li>
+                    <li class="edition-items"><i class=" bi bi-trash"></i> Delete Contact</li>
+                    <li class="edition-items "><i class="bi bi-shield-slash"></i> Block User</li>
+                </ul>
+            </div>
+        </div>
+                
+                `        
+                
+        document.querySelector('.user-info-modal').style.display = "flex"
+    
+            document.querySelector('.bi-x').addEventListener('click', () => {
+                
+            document.querySelector('.user-info-modal').style.display = "none"
+                })
+            
+    
+               
+                
+            }
+            
+    
+        })
+       
+    
+    })
+
+    window.addEventListener('click', e => {
+        if(e.target.classList == 'user-info-modal'){
+         document.querySelector('.user-info-modal').style.display = "none"
+        }
+    })
+}
+showUserInfo()
+
+
+
+
